868aed97a85f61f355c332c0699a6e5c9845cf15
dkttthai <dkttthai@gmail.com>
2017-06-30 23:19:04 +0700

    update db.sql with edit table employee and add 2 tables title,salary with relation

--- db.sql
+++ db.sql
@@ -10,25 +10,48 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
+-- Dumping database structure for tpqi
+CREATE DATABASE IF NOT EXISTS `tpqi` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci */;
+USE `tpqi`;
+
+
 -- Dumping structure for table tpqi.employee
 CREATE TABLE IF NOT EXISTS `employee` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `user` varchar(45) DEFAULT NULL,
   `pass` varchar(45) DEFAULT NULL,
-  `title` varchar(45) DEFAULT NULL,
+  `title` int(11) DEFAULT NULL,
   `firstname` varchar(45) DEFAULT NULL,
   `lastname` varchar(45) DEFAULT NULL,
   `email` varchar(45) DEFAULT NULL,
   `jobdescription` varchar(45) DEFAULT NULL,
-  `salary` int(11) DEFAULT NULL,
   `begindate` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  KEY `FK1_title` (`title`),
+  CONSTRAINT `FK1_title` FOREIGN KEY (`title`) REFERENCES `title` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- Dumping data for table tpqi.employee: ~0 rows (approximately)
 DELETE FROM `employee`;
 /*!40000 ALTER TABLE `employee` DISABLE KEYS */;
 /*!40000 ALTER TABLE `employee` ENABLE KEYS */;
+
+
+-- Dumping structure for table tpqi.salary
+CREATE TABLE IF NOT EXISTS `salary` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `value` decimal(10,2) NOT NULL DEFAULT '0.00',
+  `employee_id` int(11) NOT NULL,
+  `created_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `FK1_employee_id` (`employee_id`),
+  CONSTRAINT `FK1_employee_id` FOREIGN KEY (`employee_id`) REFERENCES `employee` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- Dumping data for table tpqi.salary: ~0 rows (approximately)
+DELETE FROM `salary`;
+/*!40000 ALTER TABLE `salary` DISABLE KEYS */;
+/*!40000 ALTER TABLE `salary` ENABLE KEYS */;
 
 
 -- Dumping structure for table tpqi.student
@@ -49,7 +72,7 @@ CREATE TABLE IF NOT EXISTS `student` (
   UNIQUE KEY `username_UNIQUE` (`username`)
 ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
 
--- Dumping data for table tpqi.student: ~0 rows (approximately)
+-- Dumping data for table tpqi.student: ~3 rows (approximately)
 DELETE FROM `student`;
 /*!40000 ALTER TABLE `student` DISABLE KEYS */;
 INSERT INTO `student` (`id`, `username`, `password`, `name`, `sname`, `code`, `major`, `faculty`, `university`, `gpa`, `gender`, `birthdate`) VALUES
@@ -57,6 +80,19 @@ INSERT INTO `student` (`id`, `username`, `password`, `name`, `sname`, `code`, `m
 	(4, 'u2', '1234', 'wat', 'sut', '5002', 'c', 'ict', 'up', 3.17, 'male', NULL),
 	(5, 'u4', '1', '2', '2', '2', '2', '2', '2', 2.00, '2', '2000-02-05 00:00:00');
 /*!40000 ALTER TABLE `student` ENABLE KEYS */;
+
+
+-- Dumping structure for table tpqi.title
+CREATE TABLE IF NOT EXISTS `title` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `value` varchar(50) COLLATE utf8_unicode_ci NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- Dumping data for table tpqi.title: ~0 rows (approximately)
+DELETE FROM `title`;
+/*!40000 ALTER TABLE `title` DISABLE KEYS */;
+/*!40000 ALTER TABLE `title` ENABLE KEYS */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
